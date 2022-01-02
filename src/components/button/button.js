fa22f8bd8ac6b67e3cccb383850940d013d04d5c
ksertas <k.serkanertas@gmail.com>
2022-01-02 18:20:45 +0100

    added navbar, missing responsiveness

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import styles from './Button.module.scss';
 
-export default function button({ children, primary, secondary, filled }) {
+export default function button({ children, primary, secondary, neutral, filled }) {
     const primaryStyle = primary ? styles.primary : '';
     const secondaryStyle = secondary ? styles.secondary : '';
-    const filledStyle = filled ? styles.filled : styles.transparent;
+    const neutralStyle = neutral ? styles.neutral : '';
+    const filledStyle = filled ? (primaryStyle ? styles.filled_primary : styles.filled_secondary) : styles.transparent;
 
     return (
         <>
             <button className={`${styles.base} 
             ${primaryStyle} 
             ${secondaryStyle}
+            ${neutralStyle}
              ${filledStyle}`}>{children}</button>
         </>
     )
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import { ReactComponent as Logo } from '../../assets/img/logo.svg';
+import styles from './NavBar.module.scss';
 import SearchInput from '../input/SearchInput';
 import Button from '../button/button';
 
 export default function NavBar() {
     return (
         <>
-            <nav>
-                <div className='nav__left'>
-                    <Logo />
+            <nav className={styles.nav}>
+                <div className={styles.nav__left}>
+                    <Logo className={styles.logo} />
                 </div>
-                <div className='nav__right'>
-                    <SearchInput />
-                    <Button secondary>Sign in</Button>
-                    <Button primary>Sign up</Button>
+                <div className={styles.nav__right}>
+                    <ul>
+                        <li><SearchInput /></li>
+                        <li><Button neutral>Sign in</Button></li>
+                        <li><Button primary>Sign up</Button></li>
+                    </ul>
                 </div>
             </nav>
         </>
