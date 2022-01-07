ca143af3398568b9a04f2c246aa5e4923e3e220d
ksertas <k.serkanertas@gmail.com>
2022-01-07 15:24:32 +0100

    added button to carousel

--- src/components/Carousel/ContentCarousel.js
+++ src/components/Carousel/ContentCarousel.js
@@ -1,21 +1,49 @@
 import React from 'react'
 import styles from './ContentCarousel.module.scss';
 import Subheader from '../media/Subheader.js';
-import Carousel, { slidesToShowPlugin, arrowsPlugin } from '@brainhubeu/react-carousel';
+import Carousel, { slidesToShowPlugin } from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 import Pidgeon from '../../assets/img/Media/pidgeon.png';
 import Discover from '../../assets/img/Media/discover.png';
 import Mountain from '../../assets/img/Media/mountain.png';
 import Tiger from '../../assets/img/Media/tiger.png';
 import MediaTile from './tiles/MediaTile';
-import { FaBeer } from 'react-icons/fa';
+import Colleagues from '../../assets/img/Media/colleagues.png';
+import Collegefriends from '../../assets/img/Media/collegefriends.png';
+import Filmsquad from '../../assets/img/Media/filmsquad.png';
+import UserTile from './tiles/UserTile';
+import GroupTile from './tiles/GroupTile';
+import { AiOutlineArrowRight, AiOutlinePlus } from 'react-icons/ai';
 
+export default function ContentCarousel({ button_type }) {
 
-export default function ContentCarousel() {
+    let icon;
+    let button;
+    switch (button_type) {
+        case "create group":
+            icon = <AiOutlinePlus />
+            button = <button className={styles.carousel__button}>Create group {icon}</button>
+            break;
+        case "add user":
+            icon = <AiOutlinePlus />
+            button = <button className={styles.carousel__button}>Add user {icon}</button>
+            break;
+        case "see all":
+            icon = <AiOutlineArrowRight />
+            button = <button className={styles.carousel__button}>See all {icon}</button>
+            break;
+
+        default:
+            icon = '';
+            button = '';
+            break;
+    }
+
     return (
         <div className={styles.carousel__container}>
             <div className={styles.title}>
                 <Subheader text="Title of tiles" />
+                {button}
             </div>
             <Carousel
                 plugins={[
@@ -29,10 +57,14 @@ export default function ContentCarousel() {
                 ]} draggable={true}
                 animationSpeed={250}>
                 {/* empty divs needed to avoid maximum update depth error */}
-                <MediaTile img={Pidgeon} title="The scary pidgeon" />
-                <MediaTile img={Discover} title="Discover" />
+                {/* <MediaTile img={Pidgeon} title="The scary pidgeon" />
+                <MediaTile img={Discover} title="Discover" /> */}
                 <MediaTile img={Mountain} title="Big tall mountains in the middle of nowhere" />
                 <MediaTile img={Tiger} title="Nature" />
+                <GroupTile img={Colleagues} name="Group 1" count={2} />
+                <GroupTile img={Collegefriends} name="College friends" count={4} />
+                <UserTile img={Filmsquad} name="Drake Strong" />
+                <UserTile img={Colleagues} name="Alice Tall" />
                 <div className={styles.invisible}></div>
                 <div className={styles.invisible}></div>
             </Carousel>
