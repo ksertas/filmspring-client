4cc5bcb5827757e727c1fbe24f9afbcc240db5cb
ksertas <k.serkanertas@gmail.com>
2022-01-09 15:23:02 +0100

    refined search input, added fixed overlay on focus

--- src/components/input/search/SearchInput.js
+++ src/components/input/search/SearchInput.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SearchInput.module.scss';
+import { ReactComponent as Search } from '../../../assets/icon/nav/ant-design_search-outlined.svg';
 
 export default function SearchInput() {
+
+    const [dimmed, setDimmed] = useState(false);
+
     return (
         <>
-            <label htmlFor="site-search" className={styles.label}>
+            <form action="" className={styles.search__form} onClick={() => setDimmed(true)}>
                 <input type="search" name="search" id="site-search" placeholder="Search" className={styles.search} />
-            </label>
+                <Search />
+            </form>
+            <div className={[`${styles.dimmer} ${dimmed ? styles.dimmer_active : ""}`]} onClick={() => setDimmed(false)}></div>
         </>
     )
 }
