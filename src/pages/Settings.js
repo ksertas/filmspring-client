c02580bd5c14e9294ece21e8298d35c90856949a
ksertas <k.serkanertas@gmail.com>
2022-01-11 21:46:54 +0100

    add first, lastname, bio, button in settings page

---
+++ src/pages/Settings.js
@@ -0,0 +1,59 @@
+// Username, Bio, Email inputs will have default values of current user's from API.
+
+import React from 'react'
+import styles from './Settings.module.scss';
+import List from '../components/Carousel/List.js';
+import Button from '../components/button/button.js';
+import { useForm } from 'react-hook-form';
+
+export default function Settings() {
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+            "firstName": "John",
+            "lastName": "Doe",
+        }
+    });
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
+    return (
+        <div className={styles.settings__container}>
+            <div className={styles.header}>
+                <h2>Account settings</h2>
+            </div>
+            <List title="Profile settings">
+                <div className={styles.settings__profile}>
+                    <form onSubmit={handleSubmit(onSubmit)} className={styles.settings__profile_personal}>
+
+                        <label htmlFor="first_name">First name</label>
+                        <input type="text" id="first_name" {...register("firstName", {
+                            required: false, pattern: {
+                                value: /^[a-z ,.'-]+$/i,
+                                message: "Name contains illegal characters"
+                            }
+                        })} />
+                        {errors.firstName && <p className={styles.input__error_message}>{errors.firstName.message}</p>}
+
+                        <label htmlFor="last_name">Last name</label>
+                        <input type="text" id="last_name" {...register("lastName", {
+                            required: false, pattern: {
+                                value: /^[a-z ,.'-]+$/i,
+                                message: "Name contains illegal characters"
+                            }
+                        })} />
+                        {errors.lastName && <p className={styles.input__error_message}>{errors.lastName.message}</p>}
+
+                        <label htmlFor="new_bio">About yourself</label>
+                        <textarea maxLength={150} type="text" id="new_bio" {...register("bio", { maxLength: { value: 150, message: "Bio must not exceed 150 characters" } })}></textarea>
+                        {errors.bio && <p className={styles.input__error_message}>{errors.bio.message}</p>}
+
+                        <Button type="submit" filled primary>Save</Button>
+                    </form>
+                </div>
+            </List>
+        </div>
+    )
+}
