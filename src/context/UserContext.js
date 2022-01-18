5e5143bdcefa0c7530d7fd43f0a48fd4d9d8a3a6
ksertas <k.serkanertas@gmail.com>
2022-01-18 18:39:50 +0100

    revamp user context with auth and user info

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,12 +1,51 @@
+import jwtDecode from "jwt-decode";
 import React, { createContext, useState } from "react";
 
-export const UserContext = createContext();
+export const UserContext = createContext({});
 
 export default function UserProvider({ children }) {
-    const [user, setUser] = useState(null);
+
+    const [auth, toggleAuth] = useState(
+        localStorage.getItem("token") ? {
+            user: {
+                username: jwtDecode(localStorage.getItem("token")).sub,
+            },
+            isAuth: true,
+        } : {}
+    )
+
+    function login(token) {
+        localStorage.setItem("token", token);
+
+        toggleAuth({
+            ...auth,
+            user: {
+                username: jwtDecode(token).sub,
+            },
+            isAuth: true
+        })
+
+    }
+
+    function logout() {
+        console.log("user is logged out");
+        localStorage.clear();
+
+        toggleAuth({
+            ...auth,
+            isAuth: false
+        })
+
+    }
+
+    const contextData = {
+        auth: auth,
+        login: login,
+        logout: logout,
+    };
 
     return (
-        <UserContext.Provider value={{ user, setUser }}>
+        <UserContext.Provider value={contextData}>
             {children}
         </UserContext.Provider>
     )
