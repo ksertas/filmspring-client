6cfcfdc8e94426ef655ab9214b83cfa5d33c8068
ksertas <k.serkanertas@gmail.com>
2022-01-19 16:19:45 +0100

    fix search page styles

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,23 +1,83 @@
 import React from 'react'
 import styles from './Search.module.scss';
-import ContentCarousel from '../../components/Carousel/ContentCarousel.js';
+import Carousel, { slidesToShowPlugin } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 
 export default function Search() {
+
+    document.body.classList.add(styles.background);
+
     return (
         <div className={styles.search__container}>
             <header className={styles.search__header}>
-                <h2>Results for {"search_term"}</h2>
+                <h4>Results for {"search_term"}</h4>
             </header>
             <main className={styles.results__container}>
-                <ContentCarousel title="Films">
+                <div className={styles.result_wrapper}>
+                    <header>
+                        <h4>Film results</h4>
+                    </header>
+                    <div>
+                        <Carousel
+                            plugins={[
+                                'arrows',
+                                {
+                                    resolve: slidesToShowPlugin,
+                                    options: {
+                                        numberOfSlides: 4
+                                    }
+                                }
+                            ]} draggable={true}
+                            animationSpeed={250}>
+                            <div></div>
+                            <div></div>
+                        </Carousel>
+                    </div>
+                </div>
 
-                </ContentCarousel>
-                <ContentCarousel title="Series">
+                <div className={styles.result_wrapper}>
+                    <header>
+                        <h4>Series results</h4>
+                    </header>
+                    <div>
+                        <Carousel
+                            plugins={[
+                                'arrows',
+                                {
+                                    resolve: slidesToShowPlugin,
+                                    options: {
+                                        numberOfSlides: 4
+                                    }
+                                }
+                            ]} draggable={true}
+                            animationSpeed={250}>
+                            <div></div>
+                            <div></div>
+                        </Carousel>
+                    </div>
+                </div>
 
-                </ContentCarousel>
-                <ContentCarousel title="Users">
-
-                </ContentCarousel>
+                <div className={styles.result_wrapper}>
+                    <header>
+                        <h4>User results</h4>
+                    </header>
+                    <div>
+                        <Carousel
+                            plugins={[
+                                'arrows',
+                                {
+                                    resolve: slidesToShowPlugin,
+                                    options: {
+                                        numberOfSlides: 4
+                                    }
+                                }
+                            ]} draggable={true}
+                            animationSpeed={250}>
+                            <div></div>
+                            <div></div>
+                        </Carousel>
+                    </div>
+                </div>
             </main>
         </div>
     )
