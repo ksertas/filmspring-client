5ac20cebad036c2154e5fa23755c000c601336c3
ksertas <k.serkanertas@gmail.com>
2022-01-19 16:26:55 +0100

    add error page styles

--- src/pages/error/Error.js
+++ src/pages/error/Error.js
@@ -3,8 +3,9 @@ import styles from './Error.module.scss';
 
 export default function Error() {
     return (
-        <div>
-            <h1>Error: Page not found</h1>
+        <div className={styles.error__container}>
+            <h1>404</h1>
+            <h2>The page you were looking for cannot be found.</h2>
         </div>
     )
 }
