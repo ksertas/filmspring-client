0ca4a1bcbc542cb899d6d96cbf85da0bb25064ee
ksertas <k.serkanertas@gmail.com>
2022-01-20 23:51:27 +0100

    add different profile headers based on privacy

--- src/components/profile_header/ProfileHeader.js
+++ src/components/profile_header/ProfileHeader.js
@@ -16,34 +16,51 @@ export default function ProfileHeader({ headerData, isCurrent }) {
                 <h4 className={styles.person__username}>@{headerData.username}</h4>
                 <p className={styles.person__bio}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, laudantium tempore! Consequatur, fuga dolores deserunt, repellat odit esse libero voluptatibus quod iusto neque eveniet, enim fugiat. Magni sapiente culpa neque.</p>
             </div>
-            <div className={styles.person__media}>
-                <div>
-                    <span>{headerData.watchedSeries.length}</span>
-                    <p>Watched series</p>
-                </div>
-                <div>
-                    <span>{headerData.watchedFilms.length}</span>
-                    <p>Watched films</p>
-                </div>
-                <div>
-                    <span>{headerData.favoriteSeries.length + headerData.favoriteFilms.length}</span>
-                    <p>Favorites</p>
-                </div>
-                <div>
-                    <span>{headerData.plannedSeries.length + headerData.plannedFilms.length}</span>
-                    <p>Plan to watch</p>
-                </div>
-            </div>
+            {headerData.mediaHidden ?
+                <p className={styles.person__media_private}>{headerData.username}'s films and series lists are private.</p>
+                :
+                <div className={styles.person__media}>
+                    <div>
+                        <span>{headerData.watchedSeries.length}</span>
+                        <p>Watched series</p>
+                    </div>
+                    <div>
+                        <span>{headerData.watchedFilms.length}</span>
+                        <p>Watched films</p>
+                    </div>
+                    <div>
+                        <span>{headerData.favoriteSeries.length + headerData.favoriteFilms.length}</span>
+                        <p>Favorites</p>
+                    </div>
+                    <div>
+                        <span>{headerData.plannedSeries.length + headerData.plannedFilms.length}</span>
+                        <p>Plan to watch</p>
+                    </div>
+                </div>}
             <div className={styles.profile__nav}>
-                <ul>
-                    <li>Overview</li>
-                    <li>Watched Films</li>
-                    <li>Watched Series</li>
-                    <li>Planned Films</li>
-                    <li>Planned Series</li>
-                    <li>Favorites</li>
-                    <li>Groups</li>
-                </ul>
+                {isCurrent ?
+                    <ul>
+                        <li>Overview</li>
+                        <li>Watched Films</li>
+                        <li>Watched Series</li>
+                        <li>Planned Films</li>
+                        <li>Planned Series</li>
+                        <li>Favorites</li>
+                        <li>Groups</li>
+                    </ul>
+                    : headerData.mediaHidden ?
+                        <ul>
+                            <li>Overview</li>
+                        </ul>
+                        :
+                        <ul>
+                            <li>Overview</li>
+                            <li>Watched Films</li>
+                            <li>Watched Series</li>
+                            <li>Planned Films</li>
+                            <li>Planned Series</li>
+                            <li>Favorites</li>
+                        </ul>}
             </div>
         </div>
     )
