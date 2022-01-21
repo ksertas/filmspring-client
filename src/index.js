b1d8197d5dc006cf560af907657fed3fd1b2d906
ksertas <k.serkanertas@gmail.com>
2022-01-21 21:32:53 +0100

    add skeleton loading theme

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import AuthProvider from './context/AuthContext';
 import UserProvider from './context/UserContext';
+import { SkeletonTheme } from 'react-loading-skeleton'
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
       <UserProvider>
-        <App />
+        <SkeletonTheme baseColor="#a37f5f" highlightColor="#d19e71" duration={2}>
+          <App />
+        </SkeletonTheme>
       </UserProvider>
     </AuthProvider>
   </React.StrictMode>,
