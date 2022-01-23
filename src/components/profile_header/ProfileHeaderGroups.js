a972b72f6e642106df005772a5793e374ccb5877
ksertas <k.serkanertas@gmail.com>
2022-01-23 15:29:17 +0100

    dynamic group profile header

--- src/components/profile_header/ProfileHeaderGroups.js
+++ src/components/profile_header/ProfileHeaderGroups.js
@@ -2,32 +2,34 @@ import React from 'react'
 import styles from './ProfileHeaderGroups.module.scss';
 import { BsFillPencilFill } from 'react-icons/bs';
 import Jake from '../../assets/img/home/jake.png';
+import Skeleton from 'react-loading-skeleton';
+import ConvertDataToImg from '../../utils/ConvertDataToImg';
 
-export default function ProfileHeaderGroups() {
+export default function ProfileHeaderGroups({ data, isGroupOwner }) {
     return (
         <div className={styles.header__container}>
             <div className={styles.header__options}>
-                <button><BsFillPencilFill />Edit group profile</button>
+                {data ?
+                    isGroupOwner ? <button><BsFillPencilFill />Edit group profile</button> : ''
+                    : ''}
             </div>
             <div className={styles.header__group}>
-                <img src={Jake} alt="" className={styles.group__img} />
-                <h3 className={styles.group__name}>Colleagues</h3>
+                {data ? <img src={ConvertDataToImg(data.avatar).src} alt="group avatar" className={styles.group__img} /> : <Skeleton circle width={150} height={150} />}
+                {data ? <h3 className={styles.group__name}>{data.groupName}</h3> : <Skeleton height={50} width={200} />}
             </div>
             <div className={styles.group__media}>
                 <div>
-                    <span>12</span>
+                    {data ? <span>{data.plannedFilms.length}</span> : <Skeleton width={30} height={20} />}
                     <p>Planned films</p>
                 </div>
                 <div>
-                    <span>32</span>
+                    {data ? <span>{data.plannedSeries.length}</span> : <Skeleton width={30} height={20} />}
                     <p>Planned series</p>
                 </div>
             </div>
             <div className={styles.group__nav}>
                 <ul>
                     <li>Overview</li>
-                    <li>Planned Films</li>
-                    <li>Planned Series</li>
                     <li>Members</li>
                 </ul>
             </div>
