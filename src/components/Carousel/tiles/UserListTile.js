53f0b24e37ba12c9906523baf7ba28afe540c6f8
ksertas <k.serkanertas@gmail.com>
2022-01-23 22:15:21 +0100

    add dynamic data to userlist tile

--- src/components/Carousel/tiles/UserListTile.js
+++ src/components/Carousel/tiles/UserListTile.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import { TiDelete } from 'react-icons/ti';
+import { ax } from '../../../api/api';
+import { UserContext } from '../../../context/UserContext';
+import ConvertDataToImg from '../../../utils/ConvertDataToImg';
 import styles from './UserListTile.module.scss';
-import John from '../../../assets/img/home/john.png'
-import { TiDelete } from 'react-icons/ti'
 
-export default function UserListTile() {
+export default function UserListTile({ groupData, userData, isGroupOwner }) {
+
+    const { user } = useContext(UserContext);
+
+    const [isRemoved, setRemoved] = useState(false);
+
+    async function removeUserFromGroup() {
+        try {
+            let res = await ax.delete(`/groups/${groupData.id}/users/${userData.username}`)
+            window.location.reload();
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     return (
         <div className={styles.tile__container}>
-            <div className={styles.tile__user}>
-                <img src={John} alt="" />
-                <p className={styles.user__info}>John smith <span className={styles.user_role}>Group owner</span></p>
-            </div>
-            <div className={styles.tile__actions}>
-                <button className={styles.user__action_btn}><TiDelete /> Remove from group</button>
-            </div>
+            {groupData && userData ? <>
+                <div className={styles.tile__user}>
+                    <img src={ConvertDataToImg(userData.avatar).src} alt="Group member avatar" className={styles.avatar} />
+                    <p className={styles.user__info}>{userData.firstName} {userData.lastName}<span className={styles.user_role}>{groupData.groupOwnerName === userData.username ? "Group owner" : "Member"}</span></p>
+                </div>
+                <div className={styles.tile__actions}>
+                    {userData.username !== user.username && isGroupOwner ?
+                        <button onClick={removeUserFromGroup} className={styles.user__action_btn}><TiDelete /> Remove from group</button> :
+                        ''}
+                </div>
+            </>
+                :
+                ''}
         </div>
     )
 }
