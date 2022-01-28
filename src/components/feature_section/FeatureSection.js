06ef96f35ce7fc627b8b0e3320539983a6227c05
ksertas <k.serkanertas@gmail.com>
2022-01-28 15:08:06 +0100

    replace filler content with features text

--- src/components/feature_section/FeatureSection.js
+++ src/components/feature_section/FeatureSection.js
@@ -8,9 +8,9 @@ import socialmedia from '../../assets/img/home/socialmedia.jpg';
 export default function FeatureSection() {
     return (
         <div className={styles.section_container}>
-            <Feature header="Feature that does something beneficial" img={movieposters}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis itaque optio impedit ut, aspernatur pariatur nesciunt expedita nostrum, illo facilis quidem! Maxime nemo deserunt odio iusto quam quidem est, rem cum! Ipsa natus, eum eaque delectus omnis modi consectetur quam!</Feature>
-            <Feature header="Share your favorite movies and series" img={devices}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe dolores quis vero nobis quod consequuntur id delectus recusandae aperiam illo! Temporibus doloribus atque dignissimos impedit laudantium qui quisquam, sapiente maiores!</Feature>
-            <Feature header="Keep track of your favorites" img={socialmedia}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, autem exercitationem! Ipsa id odio dolor! Eius, natus? Incidunt dolore</Feature>
+            <Feature header="New films and series added daily" img={movieposters}>Filmspring's movie and series database is growing rapidly with the new additions all the time.</Feature>
+            <Feature header="Access your lists from anywhere at anytime" img={devices}>Being a website, you can access Filmspring on any device with an internet connection abd browser, so you can effortlessly update your lists.</Feature>
+            <Feature header="Share with your friends" img={socialmedia}>Share links to your Filmspring profile to let your friends know what you're up to - or better yet, create a group so you can plan your next movie night with friends together.</Feature>
         </div>
     )
 }
