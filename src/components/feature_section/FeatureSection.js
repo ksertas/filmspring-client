5dc073ccbb418ca027cc8bd9954c7ed931185f8b
ksertas <k.serkanertas@gmail.com>
2022-01-28 15:39:52 +0100

    add fade animation with react-reveal

--- src/components/feature_section/FeatureSection.js
+++ src/components/feature_section/FeatureSection.js
@@ -4,13 +4,20 @@ import Feature from './Feature'
 import movieposters from '../../assets/img/home/movieposters.jpg';
 import devices from '../../assets/img/home/devices.jpg';
 import socialmedia from '../../assets/img/home/socialmedia.jpg';
+import Fade from 'react-reveal/Fade';
 
 export default function FeatureSection() {
     return (
         <div className={styles.section_container}>
-            <Feature header="New films and series added daily" img={movieposters}>Filmspring's movie and series database is growing rapidly with the new additions all the time.</Feature>
-            <Feature header="Access your lists from anywhere at anytime" img={devices}>Being a website, you can access Filmspring on any device with an internet connection abd browser, so you can effortlessly update your lists.</Feature>
-            <Feature header="Share with your friends" img={socialmedia}>Share links to your Filmspring profile to let your friends know what you're up to - or better yet, create a group so you can plan your next movie night with friends together.</Feature>
+            <Fade bottom>
+                <Feature header="New films and series added daily" img={movieposters}>Filmspring's movie and series database is growing rapidly with the new additions all the time.</Feature>
+            </Fade>
+            <Fade bottom>
+                <Feature header="Access your lists from anywhere at anytime" img={devices}>Being a website, you can access Filmspring on any device with an internet connection abd browser, so you can effortlessly update your lists.</Feature>
+            </Fade>
+            <Fade bottom>
+                <Feature header="Share with your friends" img={socialmedia}>Share links to your Filmspring profile to let your friends know what you're up to - or better yet, create a group so you can plan your next movie night with friends together.</Feature>
+            </Fade>
         </div>
     )
 }
